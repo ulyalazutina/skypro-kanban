aa15c4e0254800a937c49114c3126414601a7b78
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-01-03 09:59:06 +0300

    отображение pop-up при нажатии на имя пользователя

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleOpen() {
+        setIsOpen((prevValue) => !prevValue);
+    }
+
     return (
         <header className="header">
             <div className="container">
@@ -17,24 +24,25 @@ function Header() {
                         <button className="header__btn-main-new _hover01" id="btnMainNew">
                             <a href="#popNewCard">Создать новую задачу</a>
                         </button>
-                        <a href="#user-set-target" className="header__user _hover02">
+                        <a className="header__user _hover02" onClick={toggleOpen}>
                             Ivan Ivanov
                         </a>
-                        <div
-                            className="header__pop-user-set pop-user-set"
-                            id="user-set-target"
-                        >
-                            {/* <a href="">x</a> */}
-                            <p className="pop-user-set__name">Ivan Ivanov</p>
-                            <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                            <div className="pop-user-set__theme">
-                                <p>Темная тема</p>
-                                <input type="checkbox" className="checkbox" name="checkbox" />
+                        {isOpen && (
+                            <div
+                                className="header__pop-user-set pop-user-set"
+                                id="user-set-target"
+                            >
+                                <p className="pop-user-set__name">Ivan Ivanov</p>
+                                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                                <div className="pop-user-set__theme">
+                                    <p>Темная тема</p>
+                                    <input type="checkbox" className="checkbox" name="checkbox" />
+                                </div>
+                                <button type="button" className="_hover03">
+                                    <a href="#popExit">Выйти</a>
+                                </button>
                             </div>
-                            <button type="button" className="_hover03">
-                                <a href="#popExit">Выйти</a>
-                            </button>
-                        </div>
+                        )}
                     </nav>
                 </div>
             </div>
