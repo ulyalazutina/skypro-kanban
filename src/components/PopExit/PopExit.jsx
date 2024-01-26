6780c20c0c6a776094f94ae62a1f06fcc72f5d00
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-01-27 01:36:22 +0300

    Перенесла страницу выхода в styled components

--- src/components/PopExit/PopExit.jsx
+++ src/components/PopExit/PopExit.jsx
@@ -1,28 +1,42 @@
-import { Link } from "react-router-dom";
 import { appRoutes } from "../../lib/appRoutes";
+import {
+  PopExitWrap,
+  PopExitContainer,
+  PopExitBlock,
+  PopExitTittleWrap,
+  PopExitTittle,
+  PopExitForm,
+  PopExitFormGroup,
+  PopExitYes,
+  PopExitYesLink,
+  PopExitNo,
+  PopExitNoLink,
+} from "./PopExit.styled";
 
 function PopExit() {
-    return (
-        <div className="pop-exit" id="popExit">
-            <div className="pop-exit__container">
-                <div className="pop-exit__block">
-                    <div className="pop-exit__ttl">
-                        <h2>Выйти из аккаунта?</h2>
-                    </div>
-                    <form className="pop-exit__form" id="formExit" action="#">
-                        <div className="pop-exit__form-group">
-                            <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                                <Link to={appRoutes.SIGN_IN}>Да, выйти</Link>
-                            </button>
-                            <button className="pop-exit__exit-no _hover03" id="exitNo">
-                                <Link to={appRoutes.HOME}>Нет, остаться</Link>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <PopExitWrap>
+      <PopExitContainer>
+        <PopExitBlock>
+          <PopExitTittleWrap>
+            <PopExitTittle>Выйти из аккаунта?</PopExitTittle>
+          </PopExitTittleWrap>
+          <PopExitForm id="formExit" action="#">
+            <PopExitFormGroup>
+              <PopExitYes id="exitYes">
+                <PopExitYesLink to={appRoutes.SIGN_IN}>
+                  Да, выйти
+                </PopExitYesLink>
+              </PopExitYes>
+              <PopExitNo id="exitNo">
+                <PopExitNoLink to={appRoutes.HOME}>Нет, остаться</PopExitNoLink>
+              </PopExitNo>
+            </PopExitFormGroup>
+          </PopExitForm>
+        </PopExitBlock>
+      </PopExitContainer>
+    </PopExitWrap>
+  );
 }
 
 export default PopExit;
