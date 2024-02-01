0bbac7b7cb39cb60b6af1f75a704e8b0d226f98e
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-02-01 18:27:31 +0300

    Чтение данных пользователя из Local Storage

--- src/contexts/user.jsx
+++ src/contexts/user.jsx
@@ -3,18 +3,24 @@ import { useNavigate } from "react-router";
 import { appRoutes } from "../lib/appRoutes";
 
 export const UserContext = createContext(null);
+// возвращает данные из локального хранилища
+const getUserFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem('user'))
+}
 
 export function UserProvider({ children }) {
   let navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(getUserFromLocalStorage());
 
   const loginUser = (user) => {
     setUserData(user);
+    localStorage.setItem('user',JSON.stringify(user));
     navigate(appRoutes.HOME);
   };
 
   const logoutUser = () => {
     setUserData(null);
+    localStorage.removeItem('user');
     navigate(appRoutes.SIGN_IN);
   };
 
